4490a53136607cef38b7edeeb8c782e3485e240e
corozco6489 <co08092020@gmail.com>
2021-09-22 16:12:25 -0500

    Pagina WEB

---
+++ index.js
@@ -0,0 +1,40 @@
+var url = "http://localhost/paginaweb/";
+
+var aplicacion = new function(){
+
+
+
+    this.leer = function(){
+        console.log("conectado a base de datos");
+    }
+
+    this.Agregar = function(){
+
+        this.correo = document.getElementById("exampleFormControlInput1");
+        this.mensaje = document.getElementById("exampleFormControlTextarea1");
+        
+        console.log(this.correo.value);
+
+        var datosEnviar={correo:this.correo.value,mensaje:this.mensaje.value};
+
+        fetch(url+"?insertar=1",{method:"POST",body:JSON.stringify(datosEnviar)})
+        .then(respuesta=>respuesta.json)
+        .then((datosRespuestas)=>{
+            console.log("Insertados");
+
+            alertify.success('Mensaje Enviado');
+
+            this.mensaje.value="";
+            this.correo.value="";
+
+        })
+        .catch(console.log);
+
+
+        
+    };
+
+
+}
+aplicacion.leer();
+
---
+++ registros.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-09-2021 a las 23:10:39
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `paginaweb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registros`
+--
+
+CREATE TABLE `registros` (
+  `id` int(11) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `mensaje` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `registros`
+--
+
+INSERT INTO `registros` (`id`, `correo`, `mensaje`) VALUES
+(1, 'sdasfsafasfsa', 'orozco@gmail.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `registros`
+--
+ALTER TABLE `registros`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `registros`
+--
+ALTER TABLE `registros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
